1b1e904e10c038b0581d1bf25df94e961223c72f
zhengzaihong <1096877329@qq.com>
2022-06-12 16:05:29 +0800

    优化

--- lib/often/time_view.dart
+++ lib/often/time_view.dart
@@ -10,21 +10,23 @@ import 'package:flutter/material.dart';
 /// 使用规范: 外部请不要将 TimeViewState 持久化。
 ///
 
-typedef Child = Widget Function(
-    BuildContext context, TimeViewState controller, int time);
+typedef Child = Widget Function(BuildContext context, TimeViewState controller, int time);
 
 class TimeView extends StatefulWidget {
-
   /// 倒计时的秒数
   final int countdown;
 
   ///返回子控件的回调
   final Child? child;
 
-  const TimeView({
-    required this.countdown,
-    required this.child,
-    Key? key})
+  ///时间单位
+  final Duration duration;
+
+  const TimeView(
+      {required this.countdown,
+      required this.child,
+      this.duration = const Duration(seconds: 1),
+      Key? key})
       : super(key: key);
 
   @override
@@ -35,9 +37,8 @@ class TimeViewState extends State<TimeView> {
   /// 倒计时的计时器。
   Timer? _timer;
 
-
-  /// 当前倒计时的秒数。
-  int _seconds = 0;
+  /// 当前倒计时的时间
+  int _currentTime = 0;
 
   ///提供给外部的控制器
   late TimeViewState _controller;
@@ -45,42 +46,40 @@ class TimeViewState extends State<TimeView> {
   @override
   void initState() {
     super.initState();
-
     _controller = this;
   }
 
-  get isAvailable => _seconds<=0;
+  get isAvailable => _currentTime <= 0;
 
   /// 启动倒计时的计时器。
   void startTimer() {
-    _seconds = widget.countdown;
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (_seconds == 0) {
+    _currentTime = widget.countdown;
+    _timer = Timer.periodic(widget.duration, (timer) {
+      if (_currentTime == 0) {
         cancelTimer();
         notyChange();
         return;
       }
-      _seconds--;
-     notyChange();
+      _currentTime--;
+      notyChange();
     });
   }
 
   /// 取消倒计时的计时器。
   void cancelTimer() {
     _timer?.cancel();
-    _seconds = 0;
+    _currentTime = 0;
     notyChange();
   }
 
-  void notyChange(){
-   if(mounted){
-     setState(() {
-
-     });
-   }
+  void notyChange() {
+    if (mounted) {
+      setState(() {});
+    }
   }
+
   @override
   Widget build(BuildContext context) {
-    return widget.child!.call(context, _controller, _seconds);
+    return widget.child!.call(context, _controller, _currentTime);
   }
 }
