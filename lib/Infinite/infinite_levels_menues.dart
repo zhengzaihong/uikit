263621330d7b9a51ba9a0d63551f6292eddaf937
zhengzaihong <1096877329@qq.com>
2024-02-01 14:09:00 +0800

    '新增和优化ing'

--- lib/Infinite/infinite_levels_menues.dart
+++ lib/Infinite/infinite_levels_menues.dart
@@ -1,6 +1,3 @@
-
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_uikit_forzzh/uikitlib.dart';
 
@@ -12,105 +9,97 @@ import 'package:flutter_uikit_forzzh/uikitlib.dart';
 /// describe: 纵向无限层级菜单
 ///
 
-typedef BuildMenuItem = Widget Function(
-    InfiniteLevelsMenusState state,
-    bool isCurrent,
-    InfiniteMenu data,
-    int currentLevel);
+typedef BuildMenuItem = Widget Function(InfiniteLevelsMenusState state,
+    bool isCurrent, InfiniteMenu data, int currentLevel);
 
 typedef BuildSeparator<Dynamic> = Widget Function(
     BuildContext context, dynamic data, int currentLevel);
 
-
 class InfiniteLevelsMenus extends StatefulWidget {
-
+  /// 菜单数据
   final List<InfiniteMenu>? datas;
+
+  /// 构建样式回调
   final BuildMenuItem buildMenuItem;
+
+  /// 构建分割线
   final BuildSeparator? buildSeparator;
+
+  /// 没数据样式
   final Widget? noDataView;
+
+  /// 子菜单层级间距
   final double titleChildSpace;
-  final Function(InfiniteLevelsMenusState state,InfiniteMenu? item)? buildComplete;
+
+  /// 构建完成
+  final Function(InfiniteLevelsMenusState state, InfiniteMenu? item)?
+      buildComplete;
+
+  /// 是否展开所有
   final bool? allExpand;
+
+  /// 默认展开的那个子项菜单
   final InfiniteMenu? defaultExpand;
 
   const InfiniteLevelsMenus(
-      {
-        required this.buildMenuItem,
-        this.buildSeparator,
-        this.datas,
-        this.noDataView,
-        this.titleChildSpace =5,
-        this.buildComplete,
-        this.allExpand,
-        this.defaultExpand,
-        Key? key
-      }) : super(key: key);
+      {required this.buildMenuItem,
+      this.buildSeparator,
+      this.datas,
+      this.noDataView,
+      this.titleChildSpace = 5,
+      this.buildComplete,
+      this.allExpand,
+      this.defaultExpand,
+      Key? key})
+      : super(key: key);
 
   @override
   State<InfiniteLevelsMenus> createState() => InfiniteLevelsMenusState();
 }
 
 class InfiniteLevelsMenusState extends State<InfiniteLevelsMenus> {
-
   InfiniteMenu? _lastClickItem;
   bool isFirstBuild = true;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
+
     _lastClickItem = widget.defaultExpand;
 
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-
-      if(isFirstBuild){
+      if (isFirstBuild) {
         isFirstBuild = false;
-        widget.buildComplete?.call(this,_lastClickItem);
+        widget.buildComplete?.call(this, _lastClickItem);
       }
-      if( widget.allExpand == true){
+      if (widget.allExpand == true) {
         openAll();
         return;
       }
-      if( widget.allExpand == false){
+      if (widget.allExpand == false) {
         closeAll();
         return;
       }
-      if(_lastClickItem!=null) {
-       ///根据当前选中的层级查询所在父层级
-       setState(() {
-         var datas = widget.datas??[];
-         for(int i =0 ;i<datas.length;i++){
-           List<InfiniteMenu> parents = [];
-           var bean = datas[i];
-           parents.add(bean);
-           List<InfiniteMenu> newParents = _findAllParents(bean,parents);
-
-           if(newParents.contains(_lastClickItem)){
-             int index = newParents.indexOf(_lastClickItem!);
-             newParents.removeRange(index+1, newParents.length);
-             newParents.forEach((element) {
-               element.isChecked = true;
-               print("---------element========${element.title}");
-             });
-           }
-
-
-         }
-       });
+      if (_lastClickItem != null) {
+        ///根据当前选中的层级查询所在父层级
+        setState(() {
+          var datas = widget.datas ?? [];
+          for (int i = 0; i < datas.length; i++) {
+            List<InfiniteMenu> parents = [];
+            var bean = datas[i];
+            parents.add(bean);
+            List<InfiniteMenu> newParents = _findAllParents(bean, parents);
+            if (newParents.contains(_lastClickItem)) {
+              int index = newParents.indexOf(_lastClickItem!);
+              newParents.removeRange(index + 1, newParents.length);
+              for (var element in newParents) {
+                element.isChecked = true;
+              }
+            }
+          }
+        });
       }
     });
-  }
-
-  List<InfiniteMenu> _findAllParents(InfiniteMenu data, List<InfiniteMenu> parents){
-    var datas = data.children??[];
-    for(int i =0 ;i<datas.length;i++){
-      var bean = datas[i];
-      parents.add(bean);
-      if(bean == _lastClickItem){
-        return parents;
-      }
-      _findAllParents(datas[i],parents);
-    }
-    return parents;
   }
 
   @override
@@ -118,23 +107,20 @@ class InfiniteLevelsMenusState extends State<InfiniteLevelsMenus> {
     return buildContent();
   }
 
-  Widget buildContent(){
-    if(widget.datas==null || widget.datas!.isEmpty){
-      return  widget.noDataView==null?const SizedBox():widget.noDataView!;
+  Widget buildContent() {
+    if (widget.datas == null || widget.datas!.isEmpty) {
+      return widget.noDataView == null ? const SizedBox() : widget.noDataView!;
     }
 
     Widget infinite = Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-      ...buildTitle(widget.datas??[], 1)
-    ]);
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [...buildTitle(widget.datas ?? [], 1)]);
 
     return infinite;
-
   }
 
-  Widget buildChild(InfiniteMenu rootMenu,InfiniteMenu parentMenu,int level){
-    List<InfiniteMenu> currentData = parentMenu.children??[];
+  Widget buildChild(InfiniteMenu rootMenu, InfiniteMenu parentMenu, int level) {
+    List<InfiniteMenu> currentData = parentMenu.children ?? [];
     Widget parent = Column(
       children: [
         ListView.separated(
@@ -142,79 +128,93 @@ class InfiniteLevelsMenusState extends State<InfiniteLevelsMenus> {
             controller: ScrollController(),
             physics: const NeverScrollableScrollPhysics(),
             itemCount: currentData.length,
-            itemBuilder: (context,index){
+            itemBuilder: (context, index) {
               InfiniteMenu bean = currentData[index];
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  parentMenu.isChecked?widget.buildMenuItem.call(
-                      this,
-                      bean == _lastClickItem,
-                      bean,
-                      level):const SizedBox(),
-
-                  parentMenu.isChecked?
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(child:Padding(
-                        padding: EdgeInsets.only(left: widget.titleChildSpace*(level+1)),
-                        child:  buildChild(rootMenu,bean,level+1),
-                      ))
-                    ],
-                  )
-                      :const SizedBox(),
+                  parentMenu.isChecked
+                      ? widget.buildMenuItem
+                          .call(this, bean == _lastClickItem, bean, level)
+                      : const SizedBox(),
+                  parentMenu.isChecked
+                      ? Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                                child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: widget.titleChildSpace * (level + 1)),
+                              child: buildChild(rootMenu, bean, level + 1),
+                            ))
+                          ],
+                        )
+                      : const SizedBox(),
                 ],
               );
             },
-            separatorBuilder:(context,index){
-              return widget.buildSeparator?.call(context,currentData[index],level)??const SizedBox();
+            separatorBuilder: (context, index) {
+              return widget.buildSeparator
+                      ?.call(context, currentData[index], level) ??
+                  const SizedBox();
             })
       ],
     );
-    return parentMenu.isChecked?parent:const SizedBox();
+    return parentMenu.isChecked ? parent : const SizedBox();
   }
 
-  List<Widget> buildTitle(List<InfiniteMenu> titleMenus,int level){
+  List<Widget> buildTitle(List<InfiniteMenu> titleMenus, int level) {
     List<Widget> titles = [];
     for (var element in titleMenus) {
-      titles.add(
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-             ///标题
-            widget.buildMenuItem.call(
-                this,
-                element == _lastClickItem,
-                element,
-                level),
-              ///子标题
-              Row(
-                children: [
-                  Expanded(child:Padding(
-                    padding: EdgeInsets.only(left: widget.titleChildSpace*(level+1)),
-                    child:  buildChild(element,element,level+1),
-                  ))
-                ],
-              ),
-          ],
-        )
-      );
+      titles.add(Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ///标题
+          widget.buildMenuItem
+              .call(this, element == _lastClickItem, element, level),
+
+          ///子标题
+          Row(
+            children: [
+              Expanded(
+                  child: Padding(
+                padding:
+                    EdgeInsets.only(left: widget.titleChildSpace * (level + 1)),
+                child: buildChild(element, element, level + 1),
+              ))
+            ],
+          ),
+        ],
+      ));
     }
     return titles;
+  }
+
+  List<InfiniteMenu> _findAllParents(
+      InfiniteMenu data, List<InfiniteMenu> parents) {
+    var datas = data.children ?? [];
+    for (int i = 0; i < datas.length; i++) {
+      var bean = datas[i];
+      parents.add(bean);
+      if (bean == _lastClickItem) {
+        return parents;
+      }
+      _findAllParents(datas[i], parents);
+    }
+    return parents;
   }
 
   void setItem(InfiniteMenu data) {
     setState(() {
       data.isChecked = !data.isChecked;
-      if(!data.isChecked){
+      if (!data.isChecked) {
         _closeAllChild(data);
       }
       _lastClickItem = data;
     });
   }
 
-  void openAll(){
+  void openAll() {
     setState(() {
       widget.datas?.forEach((element) {
         element.isChecked = true;
@@ -223,7 +223,7 @@ class InfiniteLevelsMenusState extends State<InfiniteLevelsMenus> {
     });
   }
 
-  void closeAll(){
+  void closeAll() {
     setState(() {
       widget.datas?.forEach((element) {
         element.isChecked = false;
@@ -232,19 +232,17 @@ class InfiniteLevelsMenusState extends State<InfiniteLevelsMenus> {
     });
   }
 
-  void _closeAllChild(InfiniteMenu data){
+  void _closeAllChild(InfiniteMenu data) {
     data.children?.forEach((element) {
       element.isChecked = false;
       _closeAllChild(element);
     });
   }
 
-  void _openAllChild(InfiniteMenu data){
+  void _openAllChild(InfiniteMenu data) {
     data.children?.forEach((element) {
       element.isChecked = true;
       _openAllChild(element);
     });
   }
-
-
 }
