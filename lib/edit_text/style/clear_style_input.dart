e0e8e2036446bbaee0620edfd47a56203f2e0ae3
zhengzaihong <1096877329@qq.com>
2024-04-22 22:15:23 +0800

    "优化中"

--- lib/edit_text/style/clear_style_input.dart
+++ lib/edit_text/style/clear_style_input.dart
@@ -117,7 +117,7 @@ class ClearStyleInput extends InputStyleFactory {
       alignLabelWithHint: alignLabelWithHint,
       constraints: constraints,
       suffixIcon: (state.getHasContent() && enableClear && state.getIsEnable())
-          ? InkWell(
+          ? GestureDetector(
             onTap: (() {
               state.clearContent();
             }),
