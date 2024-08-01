1623946a879b276dfbd021d1fd02c1e92cc9efcf
zhengzaihong <1096877329@qq.com>
2024-08-01 17:00:52 +0800

    双向表格优化

--- lib/utils/event_notifier.dart
+++ lib/utils/event_notifier.dart
@@ -12,33 +12,65 @@ import 'package:flutter/foundation.dart';
 ///
 class EventNotifier{
 
-
-  static final Map<dynamic, ValueNotifier<dynamic>> _listenerMap = HashMap();
+  static final Map<dynamic, ValueNotifier> _listenerMap = HashMap();
+  static final Map<dynamic, List<Function(dynamic)>> _listener = HashMap();
 
   EventNotifier._();
 
-
-  static ValueNotifier<dynamic>? get(dynamic target) {
+  static ValueNotifier? get(dynamic target) {
     return _listenerMap[target];
   }
 
-  static ValueNotifier obtain(dynamic target) {
+  static ValueNotifier obtain<T>(dynamic target,{Function(dynamic)? listener,T? defaultValue}) {
     if (!_listenerMap.containsKey(target)) {
-      _listenerMap[target] = ValueNotifier(null);
+      final notifier = ValueNotifier<T?>(defaultValue);
+      _listenerMap[target] = notifier;
+      _listener[target] = [];
+      notifier.addListener(() {
+        _listener[target]?.forEach((element) {
+          element(notifier.value);
+        });
+      });
+    }
+    if(null != listener){
+     _listener[target]!.add(listener);
     }
     return _listenerMap[target]!;
   }
 
 
-  static void sendNotification(dynamic target, dynamic object) {
+  static void addListener(dynamic target,Function(dynamic) listener) {
+    if (_listener.containsKey(target)) {
+      _listener[target]!.add(listener);
+    }
+  }
+
+  static void removeListener(dynamic target,Function(dynamic) listener) {
+    if (_listener.containsKey(target)) {
+      _listener[target]!.remove(listener);
+    }
+  }
+
+
+  static void sendNotification<T>(dynamic target, T? object) {
     if (_listenerMap.containsKey(target)) {
       _listenerMap[target]!.value = object;
     }
   }
 
+  ///注意： 当有多个地方使用同一个ValueNotifier时，注意回收的时机。
   static void remove(dynamic target) {
     if (_listenerMap.containsKey(target)) {
       _listenerMap.remove(target);
     }
+
+    if (_listener.containsKey(target)) {
+      _listener.remove(target);
+    }
+  }
+
+  static void clear(){
+    _listenerMap.clear();
+    _listener.clear();
   }
 }
--- lib/utils/input_ext.dart
+++ lib/utils/input_ext.dart
@@ -33,4 +33,31 @@ extension ThrottleExtension on Function {
       });
     };
   }
+
+  void Function(dynamic value) throttle1({int milliseconds = 500}) {
+    var isAllowed = true;
+    Timer? throttleTimer;
+    return (value) {
+      if (!isAllowed) return;
+      isAllowed = false;
+      this();
+      throttleTimer?.cancel();
+      throttleTimer = Timer(Duration(milliseconds: milliseconds), () {
+        isAllowed = true;
+      });
+    };
+  }
+  void Function(dynamic value1,dynamic value2) throttle2({int milliseconds = 500}) {
+    var isAllowed = true;
+    Timer? throttleTimer;
+    return (value1,value2,) {
+      if (!isAllowed) return;
+      isAllowed = false;
+      this();
+      throttleTimer?.cancel();
+      throttleTimer = Timer(Duration(milliseconds: milliseconds), () {
+        isAllowed = true;
+      });
+    };
+  }
 }
